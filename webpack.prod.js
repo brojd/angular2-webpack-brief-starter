6b149af6900553002a6cb00845e936dc497f0a45
Dominik Broj <dominik.broj@dataart.com>
2017-02-16 15:18:13 +0100

    create configuration file for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,85 @@
-/**
- * Created by dbroj on 2017-02-15.
- */
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpack = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const rootDir = path.resolve(__dirname);
+
+module.exports = {
+  
+  entry: {
+    'js/boot': [path.resolve(rootDir, 'src', 'boot')],
+    'js/vendor': [path.resolve(rootDir, 'src', 'vendor')]
+  },
+  
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(rootDir, 'build')
+  },
+  
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(pcss|css)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'postcss-loader']),
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif)$/,
+        loader: 'file-loader?name=media/[name].[ext]',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/,
+        loader: 'raw-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  
+  debug: false,
+  
+  plugins: [
+    new HtmlWebpack({
+      filename: 'index.html',
+      inject: 'body',
+      template: path.resolve(rootDir, 'src', 'index.html')
+    }),
+    new ExtractTextPlugin("css/styles.css"),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true
+      },
+      compress: {
+        screw_ie8: true
+      },
+      comments: false
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'js/vendor',
+      minChunks: Infinity
+    })
+  ],
+  
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  }
+  
+};
