004dde48019cf81b56eab5d4a98364d6e474d787
Dominik Broj <dominik.broj@dataart.com>
2017-02-21 13:12:27 +0100

    create and use pikachu component

--- src/app/feature_modules/clock/index.ts
+++ src/app/feature_modules/clock/index.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
+
+import { SharedModule } from '../../shared_module';
 import { ClockService } from './services/clock.service';
 import { TimerComponent } from './components/timer/timer.component';
 
 @NgModule({
+  imports: [ CommonModule, HttpModule, SharedModule ],
   declarations: [ TimerComponent ],
   exports: [ TimerComponent ],
   providers: [ ClockService ]
--- src/app/feature_modules/yourIP/index.ts
+++ src/app/feature_modules/yourIP/index.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
 
+import { SharedModule } from '../../shared_module';
 import { YourIPService } from './services/yourIP.service';
 import { YourIPComponent } from './components/yourIP/yourIP.component';
 
 @NgModule({
-  imports: [ RouterModule, CommonModule, HttpModule ],
+  imports: [ CommonModule, HttpModule, SharedModule ],
   declarations: [ YourIPComponent ],
   exports: [ YourIPComponent ],
   providers: [ YourIPService ]
